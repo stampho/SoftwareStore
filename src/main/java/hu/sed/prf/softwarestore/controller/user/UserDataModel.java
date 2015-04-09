edb0409867291903e9ee2df195de1d3b758598fd
Peter Varga <pvarga@inf.u-szeged.hu>
2015-04-09 18:46:15 +0200

    Add user table to the main page

--- src/main/java/hu/sed/prf/softwarestore/controller/user/UserDataModel.java
+++ src/main/java/hu/sed/prf/softwarestore/controller/user/UserDataModel.java
@@ -1,13 +1,19 @@
 package hu.sed.prf.softwarestore.controller.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import hu.sed.prf.softwarestore.controller.core.AbstractDataModel;
 import hu.sed.prf.softwarestore.dao.core.AbstractGenericDAO;
 import hu.sed.prf.softwarestore.dao.user.UserDAO;
 import hu.sed.prf.softwarestore.entity.user.User;
 
-
+@Named
+@ViewScoped
 public class UserDataModel extends AbstractDataModel<User, Long> {
 	
 	private static final long serialVersionUID = 599328296781344887L;
@@ -18,6 +24,14 @@ public class UserDataModel extends AbstractDataModel<User, Long> {
 	@Override
 	protected AbstractGenericDAO<User, Long> getEntityDao() {
 		return userDAO;
+	}
+
+	// TODO(pvarga): Remove this! For testing purposes only.
+	@Override
+	public List<User> getList() {
+		List<User> users = new ArrayList<User>();
+		users.add(new User("user", "user@mail.com"));
+		return users;
 	}
 
 }
